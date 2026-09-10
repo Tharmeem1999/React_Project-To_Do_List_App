4d7e8cc506956935af65188e0727b79a706f1b71
tharmeem1999 <tharmeem1999@gmail.com>
2026-09-10 12:14:08 +0530

    built the TaskControls Component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,26 +2,31 @@ import TaskItem from "./TaskItem";
 // eslint-disable-next-line no-unused-vars
 import EditTaskForm from "./EditTaskForm";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({
+    tasks,
+    showOnlyIncomplete,
+}) => {
     return (
         <ul>
-            {tasks.map((task) => (
-                <li
-                    key={task.id}
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        padding: "10px",
-                        borderBottom: "1px solid grey",
-                        gap: "10px"
-                    }}
-                >
-                    <TaskItem
-                        task={task}
-                    />
-                </li>
-            ))}
+            {tasks
+                .filter((task) => !showOnlyIncomplete || !task.done)
+                .map((task) => (
+                    <li
+                        key={task.id}
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            padding: "10px",
+                            borderBottom: "1px solid grey",
+                            gap: "10px"
+                        }}
+                    >
+                        <TaskItem
+                            task={task}
+                        />
+                    </li>
+                ))}
         </ul>
     );
 };
